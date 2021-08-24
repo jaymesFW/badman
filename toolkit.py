898ea42fb957cd82bbe4c21edafad97e6df75258
jaymesFW <89203454+jaymesFW@users.noreply.github.com>
2021-08-24 11:47:36 +0100

    Update toolkit.py

--- toolkit.py
+++ toolkit.py
@@ -101,4 +101,4 @@ st.title(f"Toolkit Builder")
 # Main - dataframes
 st.markdown("### Selected Team's Stats 2020/21")
 
-st.dataframe(data.sort_values(by=[choose_metric],ascending=False).reset_index(drop=True))
+st.dataframe(data.sort_values(by=["Goals"],ascending=False).reset_index(drop=True))
